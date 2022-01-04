c06382c4d966d0478217d3c5d562fdcb4c321295
Chava123001 <45792639+Chava123001@users.noreply.github.com>
2022-01-04 14:00:30 -0800

    cree el css
    css

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
+    <link rel="stylesheet" type="text/css" href="estilos/estilos.css">
 </head>
 <body>
+    <div id="buscador">
+
+
+    <form action="procesa.php" method="POST">
+        <input type="text" >
+        <input type="button" value="BUSCAR">
+        
+
+
+
+
+    </form>
+
+
+    
+    </div>
+    <h3>Tienda de abarrotes.com</h3>
+   
+
+    <img src="1.png"  id="carrito" alt="">
+
     
 </body>
 </html>
